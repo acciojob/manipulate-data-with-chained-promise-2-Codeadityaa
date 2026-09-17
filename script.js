8391786b134f8ad4a7fb34dc70b67d15dd162354
Aditya Raj <154418353+Codeadityaa@users.noreply.github.com>
2026-09-17 17:58:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+const outputDiv= document.getElementById("output");
+const inputArray=;
+const intialPromise = new promise((resolve)=>{
+	setTimeout(()=>
+		resolve(inputArray);},3000);
+})
+initialPromise
+    .then((array) => {
+		const evens= Array.filter(num=>num%2=0);
+		return new promise((resolve)=>){
+			setTimeout(()=>
+			resolve(evens);},1000);
+		})
+  .then((evensArray) => {
+	  const Multiplied=evensArray.map(num=>num*2);
+	   return new Promise((resolve) => {
+            setTimeout(() => {
+                outputDiv.textContent = multiplied.join(","); // Updates DOM to: 4,8
+                resolve(multiplied);
+            }, 2000);
+        });
+    });
